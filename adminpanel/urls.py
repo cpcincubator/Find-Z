c851ba780ba98f59f8ebbb96aff337c5d11cca4b
tahsiniftekhar <tahsiniftekhar@gmail.com>
2020-06-28 00:18:47 +0600

    Project Cleaned and AdminPanel added

--- adminpanel/urls.py
+++ adminpanel/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
-urlpatterns = [
-    path('', views.home),
 
+urlpatterns = [
+    path('', views.index, name="adminpanel"),
+    path('logout/', views.admin_logout, name='admin-logout'),
+    path('login/', views.admin_login, name='admin-login'),
+    path('tutorials/', views.Tutorials.as_view(), name='admin-tutorials'),
 ]
